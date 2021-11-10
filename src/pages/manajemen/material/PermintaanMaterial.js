99392df8e3bafa026c565e675f9b5752ef3a9d3b
aulusuf <aulusuf@gmail.com>
2021-11-10 19:39:42 +0700

    update

--- src/pages/manajemen/material/PermintaanMaterial.js
+++ src/pages/manajemen/material/PermintaanMaterial.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React from "react";
 
 const PermintaanMaterial = () => {
   return (
-    <div className='permintaan material'>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
-      <h1>Permintaan Material</h1>
+    <div className="content vh-100">
+      <div className="container row mx-auto pt-3 gx-3">
+        <div className="col-xl-6 col-sm-12 p-2">
+          <div className="content-box">
+            <div className="content-head mb-2">this is content head</div>
+            <div className="content-body mb-2">this is content body</div>
+          </div>
+        </div>
+        <div className="col-xl-6 col-sm-12 p-2">
+          <div className="content-box">
+            <div className="content-head mb-2">this is content head</div>
+            <div className="content-body mb-2">this is content body</div>
+          </div>
+        </div>
+        <div className="col-xl-6 col-sm-12 p-2">
+          <div className="content-box">this is the content2</div>
+        </div>
+        <h1>Permintaan Material 1</h1>
+      </div>
     </div>
   );
 };
