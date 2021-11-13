654c908bbfdd3d680caf0c8bf8468d53b2dc90ce
Abdul Hadi <abdulhadibwd@gmail.com>
2021-11-13 16:21:49 +0700

    create interface tim produksi

---
+++ src/App.js
@@ -0,0 +1,38 @@
+// routing
+
+import './App.css';
+import { Sidebar } from './components/Sidebar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/manajemen/Dashboard';
+import BuatPermintaan from './pages/manajemen/produksi/PermintaanProduksi';
+import ProsesProduksi from './pages/manajemen/produksi/ProsesProduksi';
+import SelesaiProduksi from './pages/manajemen/produksi/SelesaiProduksi';
+import TambahProduk from './pages/manajemen/products/TambahProduk';
+import DataProduk from './pages/manajemen/products/DataProduk';
+import PermintaanMaterial from './pages/manajemen/material/PermintaanMaterial';
+import StokMaterial from './pages/manajemen/material/StokMaterial';
+import Kategori from './pages/manajemen/Kategori';
+import DataPegawai from './pages/manajemen/pegawai/DataPegawai';
+
+function App() {
+  return (
+    <Router>
+      <Sidebar />
+      <Switch>
+        <Route path='/manajemen/dashboard' exact component={Dashboard} />
+        <Route path='/manajemen/produksi/permintaan' exact component={BuatPermintaan} />
+        <Route path='/manajemen/produksi/proses' exact component={ProsesProduksi} />
+        <Route path='/manajemen/produksi/selesai' exact component={SelesaiProduksi} />
+        <Route path='/manajemen/produk/tambah-produk' exact component={TambahProduk} />
+        <Route path='/manajemen/produk/data-produk' exact component={DataProduk} />
+        <Route path='/manajemen/material/permintaan' exact component={PermintaanMaterial} />
+        <Route path='/manajemen/material/stok' exact component={StokMaterial} />
+        <Route path='/manajemen/kategori' exact component={Kategori} />
+        <Route path='/manajemen/pegawai' exact component={DataPegawai} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ src/components/SidebarData.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as RiIcons from 'react-icons/ri';
+import * as GiIcons from 'react-icons/gi';
+import * as MdIcons from 'react-icons/md';
+import * as BiIcons from 'react-icons/bi';
+
+
+export const SidebarData = [
+  {
+    title: 'Dashboard',
+    path: '/manajemen/dashboard',
+    icon: <AiIcons.AiOutlineDashboard />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+  },
+  {
+    title: 'Produksi',
+    icon: <GiIcons.GiGearHammer />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: 'Buat Permintaan',
+        path: '/manajemen/produksi/permintaan',
+        icon: <GiIcons.GiGearHammer />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Proses',
+        path: '/manajemen/produksi/proses',
+        icon: <GiIcons.GiGearHammer />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Selesai',
+        path: '/manajemen/produksi/selesai',
+        icon: <GiIcons.GiGearHammer />
+      }
+    ]
+  },
+  {
+    title: 'Produk',
+    icon: <MdIcons.MdProductionQuantityLimits />,
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: 'Tambah Produk',
+        path: '/manajemen/produk/tambah-produk',
+        icon: <MdIcons.MdProductionQuantityLimits />,
+        cName: 'sub-nav'
+      },
+      {
+        title: 'Data Produk',
+        path: '/manajemen/produk/data-produk',
+        icon: <MdIcons.MdProductionQuantityLimits />,
+        cName: 'sub-nav'
+      }
+    ]
+  },
+  {
+    title: 'Material',
+    icon: <GiIcons.GiMaterialsScience />,
+
+    iconClosed: <RiIcons.RiArrowDownSFill />,
+    iconOpened: <RiIcons.RiArrowUpSFill />,
+
+    subNav: [
+      {
+        title: 'Permintaan',
+        path: '/manajemen/material/permintaan',
+        icon: <GiIcons.GiMaterialsScience />
+      },
+      {
+        title: 'Stok',
+        path: '/manajemen/material/stok',
+        icon: <GiIcons.GiMaterialsScience />
+      }
+    ]
+  },
+  {
+    title: 'Kategori',
+    path: '/manajemen/kategori',
+    icon: <FaIcons.FaEnvelopeOpenText />,
+  },
+  {
+    title: 'Pegawai',
+    path: '/manajemen/pegawai',
+    icon: <BiIcons.BiGroup style={{color:'white'}}/>,
+  },
+];
