2b69ae82d0ad4f8c542ae313c1508a9965f45fea
Muhammad Yusuf Auliya <aulusuf@gmail.com>
2021-11-16 09:54:49 +0700

    Login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,12 @@
 import React from "react";
+import "../styles/bootstrap/css/bootstrap.min.css";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
     <div className="bg-login vh-100">
       <div className="container row">
         <div className="box col-xl-4 col-md-6 col-sm-8 col-xs-10 d-flex flex-column justify-content-center">
-          <div className="overlay py-4 text-center">Produksi.in</div>
           <div className="form-box bg-white rounded px-3 py-4">
             <div className="logo pb-3">
               <h2>Produksi.in</h2>
@@ -22,8 +23,12 @@ const Login = () => {
                   <input className="form-control" placeholder="password" />
                 </div>
                 <div className="text-center pt-3">
-                  <button className="btn btn-primary">Masuk</button>
-                  <p className="py-3">Lupa password?</p>
+                  <div>
+                    <button className="btn btn-primary">Masuk</button>
+                  </div>
+                  <div>
+                    <Link className="py-3">Lupa password?</Link>
+                  </div>
                 </div>
               </form>
             </div>
