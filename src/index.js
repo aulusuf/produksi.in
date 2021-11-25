1a4d3945d7f5ea619c9d38bda2d7013cf1ad5148
Muhammad Yusuf Auliya <aulusuf@gmail.com>
2021-11-25 09:12:59 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import axios from "axios";
 
-axios.defaults.baseURL = "http://produksiin-backend.herokuapp.com/";
+axios.defaults.baseURL = "https://produksiin-backend.herokuapp.com/";
 // axios.defaults.baseURL = "http://localhost:8081/";
 
 ReactDOM.render(
